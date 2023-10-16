fbb37f72f50a2a6efdb9f5126993c4346ce89077
ThabangManaka <thabangdansteyn@gmail.com>
2023-10-16 15:28:24 +0200

    Added temperature

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,11 +8,21 @@ import { WeatherService } from '../services/weather.service';
 })
 export class HomePage {
 tempData :any;
+weatherIcon: any;
   constructor(private weatherService : WeatherService) {
  this.weatherService.getCurrent().subscribe(res => {
    this.tempData = res
+   console.log(this.tempData);
    });
  
   }
-
+  // getChanceOfRain(): number {
+  
+  //   if (this.weatherData && this.weatherData.rain) {
+     
+  //     return this.weatherData.rain['1h'] || 0;
+  //   } else {
+  //     return 0; 
+  //   }
+  // }
 }
