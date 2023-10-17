6de436b6e18300630ab2fa865b0a666958b266fb
ThabangManaka <thabangdansteyn@gmail.com>
2023-10-17 12:28:44 +0200

    Added Swiper

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,20 +9,33 @@ import { WeatherService } from '../services/weather.service';
 export class HomePage {
 tempData :any;
 weatherIcon: any;
+forecastData: any;
   constructor(private weatherService : WeatherService) {
  this.weatherService.getCurrent().subscribe(res => {
    this.tempData = res
    console.log(this.tempData);
+
+  //  this.weatherService.getForecasEveryHour(this.tempData.coord.lat,this.tempData.coord.lon).subscribe(res =>{
+  //  this.forecastData = res
+  //  console.log(this.forecastData )
+  //  })
    });
- 
+
+
+    this.weatherService.getDailyForecast().subscribe(res =>{
+
+      this.forecastData = res,
+      console.log(this.forecastData.list)
+
+    })
   }
   // getChanceOfRain(): number {
-  
+
   //   if (this.weatherData && this.weatherData.rain) {
-     
+
   //     return this.weatherData.rain['1h'] || 0;
   //   } else {
-  //     return 0; 
+  //     return 0;
   //   }
   // }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -9,9 +9,22 @@ const url = 'https://api.openweathermap.org/data/3.0/onecall?';
 })
 export class WeatherService {
   private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private forecastEndpoint = 'forecast';
   constructor(public http : HttpClient) { }
 
   getCurrent(): Observable<any>{
    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${"Cape Town"}&appid=${API_KEY}`)
   }
+
+  getForecasEveryHour(lat:any,lon:any) {
+    https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={API key}
+    return this.http.get(`https://pro.openweathermap.org/data/2.5/forecast/hourly?q=Cape Town&appid=87e9146464b43379098266fed155bb89`)
+  }
+
+  getDailyForecast() {
+
+    return this.http.get(`https://api.openweathermap.org/data/2.5/forecast?q=Cape Town&appid=87e9146464b43379098266fed155bb89`)
+  }
+
+
 }
